d8e39b736f0e062446adba62910c1cc24daf8472
Arlene Rodriguez <arlene.rodriguez1@gmail.com>
2020-11-24 16:56:47 -0500

    Added to Aboutme component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,14 +7,20 @@ function About () {
 <div class="container">
   <div class="row">
     <div class="col-sm">
-      <h3>One of three columns</h3>
-      <p></p>
+      <h4>Human Interaction</h4>
+      <p class="header">Teamwork makes the dream work.</p>
+      <p><i class="fas fa-users fa-5x"></i></p>
     </div>
     <div class="col-sm">
-      One of three columns
+      <h4>Staying Active</h4>
+      <p class ="header">Yoga, hiking, or biking? I'm there!</p>
+      <p><i class="fas fa-hiking fa-5x"></i></p>
+      
     </div>
     <div class="col-sm">
-      One of three columns
+      <h4>New Adventures</h4>
+      <p class="header">Visiting new places brings new knowledge.</p>
+      <p><i class="fas fa-luggage-cart fa-5x"></i></p>
     </div>
   </div>
 </div>
